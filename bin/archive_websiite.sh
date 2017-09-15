a090a01a83d1b94ce146a2e1f64189aa3213ed75
Federico Weber <federicoweber@gmail.com>
2017-09-14 21:45:48 -0700

    small update

---
+++ bin/archive_websiite.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Recursivelly get the entire webiste
+
+URL=$1
+LOGFILE=./backup-logs
+
+wget \
+  --header="Accept: text/html" \
+  --recursive \
+  --no-clobber \
+  --page-requisites \
+  --html-extension \
+  --convert-links \
+  --domains $URL \
+  --wait=10 --random-wait \
+  --limit-rate=500k \
+  --no-parent \
+  --user-agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" \
+  $URL \
+  | tee --append $LOGFILE
